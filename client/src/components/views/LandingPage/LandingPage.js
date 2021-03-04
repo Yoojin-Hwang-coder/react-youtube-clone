58d26e0edb38321a5e274354e9612bb27d2dead5
Yoojin Hwang <dbwlshyj89@gmail.com>
2021-03-04 22:51:07 +0900

    비디오 업로드와 썸네일 생성 기능 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -2,22 +2,6 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
 function LandingPage(props) {
-  useEffect(() => {
-    axios.get('/api/hello').then((response) => {
-      console.log(response);
-    });
-  }, []);
-
-  const onClickHandler = () => {
-    axios.get(`/api/users/logout`).then((response) => {
-      if (response.data.success) {
-        props.history.push('/login');
-      } else {
-        alert('로그아웃 하는데 실패 했습니다.');
-      }
-    });
-  };
-
   return (
     <div
       style={{
@@ -29,8 +13,6 @@ function LandingPage(props) {
       }}
     >
       <h2>시작 페이지</h2>
-
-      <button onClick={onClickHandler}>로그아웃asdfsdaf</button>
     </div>
   );
 }
